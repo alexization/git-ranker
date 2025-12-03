e530eb39c47ec01cd4831ab2bb36a0a04a93d812
hyoseok <alexization@gmail.com>
2025-12-03 15:43:39 +0900

    featrue: ActivityLog nullable 유효성 조건 추가 및 Builder 및 생성자 추가

--- src/main/java/com/gitranker/api/domain/log/ActivityLog.java
+++ src/main/java/com/gitranker/api/domain/log/ActivityLog.java
@@ -2,11 +2,17 @@ package com.gitranker.api.domain.log;
 
 import com.gitranker.api.domain.user.User;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Entity
 @Table(name = "activity_logs")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ActivityLog {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,12 +22,32 @@ public class ActivityLog {
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
+    @Column(nullable = false)
     private LocalDate activityDate;
 
-    private int commitCount;
-    private int prCount;
-    private int issueCount;
-    private int reviewCount;
+    @Column(nullable = false)
+    private int commitCount = 0;
 
-    private int dailyScore;
+    @Column(nullable = false)
+    private int prCount = 0;
+
+    @Column(nullable = false)
+    private int issueCount = 0;
+
+    @Column(nullable = false)
+    private int reviewCount = 0;
+
+    @Column(nullable = false)
+    private int dailyScore = 0;
+
+    @Builder
+    public ActivityLog(User user, LocalDate activityDate, int commitCount, int prCount, int issueCount, int reviewCount, int dailyScore) {
+        this.user = user;
+        this.activityDate = activityDate;
+        this.commitCount = commitCount;
+        this.prCount = prCount;
+        this.issueCount = issueCount;
+        this.reviewCount = reviewCount;
+        this.dailyScore = dailyScore;
+    }
 }
