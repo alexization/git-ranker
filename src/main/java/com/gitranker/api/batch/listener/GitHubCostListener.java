30856677247a29fa21610394610b0353c8eeb094
hyoseok <alexization@gmail.com>
2026-01-07 11:30:22 +0900

    refactor: listener 및 logback 로그 리팩토링

--- src/main/java/com/gitranker/api/batch/listener/GitHubCostListener.java
+++ src/main/java/com/gitranker/api/batch/listener/GitHubCostListener.java
@@ -1,5 +1,7 @@
 package com.gitranker.api.batch.listener;
 
+import com.gitranker.api.global.logging.EventType;
+import com.gitranker.api.global.logging.LogCategory;
 import com.gitranker.api.global.logging.MdcUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.JobExecution;
@@ -22,7 +24,8 @@ public class GitHubCostListener implements JobExecutionListener {
         int totalCost = jobExecution.getExecutionContext().getInt(TOTAL_COST_KEY, 0);
 
         MdcUtils.setGithubApiCost(totalCost);
-        log.info("소요 GitHub API Cost: {}", totalCost);
+        MdcUtils.setLogContext(LogCategory.BATCH, EventType.SUCCESS);
 
+        log.info("소요 GitHub API Cost: {}", totalCost);
     }
 }
