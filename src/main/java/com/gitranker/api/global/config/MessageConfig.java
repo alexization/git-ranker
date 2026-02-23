0e9ed774ec53f3df4f3d213c61bc1712fab75336
hyoseok <alexization@gmail.com>
2026-02-23 16:48:54 +0900

    feature: 국제화 메시지 인프라 추가

---
+++ src/main/java/com/gitranker/api/global/config/MessageConfig.java
@@ -0,0 +1,35 @@
+package com.gitranker.api.global.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Locale;
+
+@Configuration
+public class MessageConfig {
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasenames("classpath:messages");
+        messageSource.setDefaultEncoding(StandardCharsets.UTF_8.name());
+        messageSource.setFallbackToSystemLocale(false);
+
+        return messageSource;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.KOREAN);
+        localeResolver.setSupportedLocales(List.of(Locale.KOREAN, Locale.ENGLISH));
+
+        return localeResolver;
+    }
+}
